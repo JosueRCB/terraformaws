6d6cb89c60c95b135d558344213e496f65ed2799
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-17 16:53:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,12 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"
+  acces-key="AKIA3PZHURNFC5WTUV4H"
+  secret-key="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"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-02f3416038bdb17fb"
   instance_type = "t2.micro"
 
   tags = {
