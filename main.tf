b719991494d6cdcd29affeca6f48dac701f8de97
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-17 17:46:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = "~>= 1.2.0"
 }
 
 provider "aws" {
   region  = "us-west-2"
+  AWSSecretKey = "Vbxt54BFBg1PVgQvLgPeg7UHeanLLYHDsq3S9ars"
+  AWSAccessKeyId="AKIA3PZHURNFC5WTUV4H"
+  
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-02f3416038bdb17fb"
+resource "aws_instance" "firts-instance" {
+  ami           = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
 
   tags = {
