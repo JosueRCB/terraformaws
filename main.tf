4d73a09c2be98bdd1a1b550e9e5b81ed3e33f1e3
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-17 19:25:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-eats-1"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "proyecto" {
-  ami           = "ami-0e4d9ed95865f3b40"
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
